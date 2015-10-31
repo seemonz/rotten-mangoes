d8d118178b563816d82690321cfa4bc321102e62
seemonz <seemonz@gmail.com>
2015-10-31 01:24:48 +0000

    added admin user new page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-  # before_filter :authorize_admin
+  before_filter :authorize_admin
 
   def index
     @users = User.page(params[:page])
@@ -7,7 +7,15 @@ class Admin::UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "Created #{@user.email}"
+    end
   end
 
   def edit
@@ -35,7 +43,7 @@ class Admin::UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:email, :firstname, :lastname)
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
   end
 
 
